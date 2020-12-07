5cfcace27fc2294351b218c13aa66a76a5baeb4c
ggarredondo <ggarredondo@orreo.ugr.es>
2020-12-07 10:57:37 +0100

    cambios menores

--- luzdireccional.h
+++ luzdireccional.h
@@ -11,22 +11,20 @@ protected:
 	void rotacionEjeY() {
 		posicion[0] = posicion[2]*sin(alpha) + posicion[0]*cos(alpha);
 		posicion[2] = posicion[2]*cos(alpha) - posicion[0]*sin(alpha);
-		alpha = 0;
 	}
 	void rotacionEjeX() {
 		posicion[1] = posicion[1]*cos(beta) - posicion[2]*sin(beta);	
 		posicion[2] = posicion[1]*sin(beta) + posicion[2]*cos(beta);
-		beta = 0;
 	}
 
 public:
 	LuzDireccional(const Tupla2f& orientacion, GLenum idLuz, const Tupla4f& cdifuso, const Tupla4f& cespecular, const Tupla4f& cambiente);
 	void activar();
 	inline void variarAnguloAlpha(float incremento) {
-		alpha += incremento;
+		alpha = incremento;
 	}
 	inline void variarAnguloBeta(float incremento) {
-		beta += incremento;
+		beta = incremento;
 	}
 };
 
