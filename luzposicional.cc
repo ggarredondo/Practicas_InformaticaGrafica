fbcfce19b2ceb0aa40caa9181b858f888844dbaa
ggarredondo <ggarredondo@orreo.ugr.es>
2020-12-13 23:27:56 +0100

    Cambios menores

--- luzposicional.cc
+++ luzposicional.cc
@@ -3,7 +3,7 @@
 LuzPosicional::LuzPosicional(const Tupla3f& lposicion, GLenum idLuz, const Tupla4f& cdifuso, const Tupla4f& cespecular, const Tupla4f& cambiente)
 {
 	id = idLuz;
-	posicion = Tupla4f(lposicion[0],lposicion[1],lposicion[2], 1); //posiblemente mal
+	posicion = Tupla4f(lposicion[0],lposicion[1],lposicion[2], 1);
 	colorDifuso = cdifuso;
 	colorEspecular = cespecular;
 	colorAmbiente = cambiente;
