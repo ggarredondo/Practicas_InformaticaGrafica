3794973ebe545ff0d8ba8a561887e8404286cadd
ggarredondo <ggarredondo@orreo.ugr.es>
2020-12-20 01:40:35 +0100

    Terminado morsmanum en t-pose

--- brazodech.cc
+++ brazodech.cc
@@ -16,20 +16,20 @@ void BrazoDech::draw(dibujado d, patron p)
 		shoulder->draw(d,p);
 
 		glPushMatrix();
-			glTranslatef(-3.1,-0.4,-0.15);
+			glTranslatef(-3.72,-0.4,-0.15);
 			forearm->draw(d,p);
 
 			glPushMatrix();
-				glTranslatef(-2.8,0.1,0.65);
+				glTranslatef(-3.36,0.1,0.65);
 				hand->draw(d,p);
 
 				glPushMatrix();
-					glTranslatef(-1.8,0.2,0.15);
+					glTranslatef(-2.16,0.2,0.15);
 					fingers->draw(d,p);
 				glPopMatrix();
 
 				glPushMatrix();
-					glTranslatef(-1.5,-0.7,1.0);
+					glTranslatef(-1.8,-0.7,1.0);
 					thumb->draw(d,p);
 				glPopMatrix();
 			glPopMatrix();
--- morsmanum.cc
+++ morsmanum.cc
@@ -14,32 +14,35 @@ void Morsmanum::draw(dibujado d, patron p)
 {
 	glMatrixMode(GL_MODELVIEW);
 	glPushMatrix();
+		glTranslatef(0,70,0);
+			
+		glPushMatrix();
+			glTranslatef(0,10,-3);
+			glRotatef(120,0,1,0);
+			cabeza->draw(d,p);
+		glPopMatrix();
+
+		glScalef(13,13,13);
+		torso->draw(d,p);
 
 		glPushMatrix();
-			glScalef(13,13,13);
-			glTranslatef(0,10,0); //temp
-			torso->draw(d,p);
+			glTranslatef(1.9,-1.1,-0.1);
+			brazo_izq->draw(d,p);
+		glPopMatrix();
 
-			glPushMatrix();
-				glTranslatef(1.9,-1.1,-0.1);
-				brazo_izq->draw(d,p);
-			glPopMatrix();
+		glPushMatrix();
+			glTranslatef(-1.9,-1.1,-0.1);
+			brazo_dech->draw(d,p);
+		glPopMatrix();
 
-			glPushMatrix();
-				glTranslatef(-1.9,-1.1,-0.1);
-				brazo_dech->draw(d,p);
-			glPopMatrix();
+		glPushMatrix();
+			glTranslatef(1,-7.1,-0.7);
+			pierna_izq->draw(d,p);
+		glPopMatrix();
 
-			glPushMatrix();
-				glTranslatef(1,-7.1,-0.7);
-				pierna_izq->draw(d,p);
-			glPopMatrix();
-
-			glPushMatrix();
-				glTranslatef(-1,-7.1,-0.7);
-				pierna_dech->draw(d,p);
-			glPopMatrix();
-
+		glPushMatrix();
+			glTranslatef(-1,-7.1,-0.7);
+			pierna_dech->draw(d,p);
 		glPopMatrix();
 
 	glPopMatrix();
