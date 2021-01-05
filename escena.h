62d5a277fcf0d3278dbeb0013b5fec37b31e56bc
ggarredondo <ggarredondo@orreo.ugr.es>
2021-01-05 13:11:50 +0100

    cambios tontos

--- escena.h
+++ escena.h
@@ -80,7 +80,7 @@ class Escena
    Cono* con = nullptr;
    Esfera* sph = nullptr;
 
-   ObjRevolucion* lata = nullptr;z
+   ObjRevolucion* lata = nullptr;
 
    LuzPosicional* luzP = nullptr;
    LuzDireccional* luzD = nullptr;
